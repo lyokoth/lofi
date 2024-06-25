400a2c5c37917f792424d66d0aa34f597d791b55
Lynette Okoth <lynokoth79@gmail.com>
2024-06-25 16:08:56 -0400

    add jazz playlist

---
+++ src/data/youtubePlaylistData.js
@@ -0,0 +1,54 @@
+export const tokyo = [
+    {
+        id: 0,
+        title: "japan night drive - chill lofi hip hop to study / work / sleep + late night drive in japan ✨",
+        url: "https://www.youtube.com/watch?v=YvC4PTKoLww",
+        thumbnail: "https://i.ytimg.com/vi/1jXw9G7y3ow/maxresdefault.jpg",
+        author: "TOKYO TONES",
+    },
+    {
+        id: 1,
+        title: "🏎 tokyo night drive - synthwave radio / upbeat / chill 24/7",
+        url: "https://www.youtube.com/watch?v=KNJyQwgzrMg",
+        thumbnail: "https://i.ytimg.com/vi/KNJyQwgzrMg/maxresdefault.jpg",
+        author: "TOKYO TONES",
+    },
+];
+
+export const lofiGirl = [
+    {
+        id: 0,
+        title: "lofi hip hop radio - beats to relax/study to",
+        url: "https://www.youtube.com/watch?v=5qap5aO4i9A",
+        thumbnail: "https://i.ytimg.com/vi/5qap5aO4i9A/maxresdefault.jpg",
+        author: "Lofi Girl",
+    },
+    {
+        id: 1,
+        title: "1980s Lofi",
+        url: "https://www.youtube.com/watch?v=gcB4ay_fO1k",
+        thumbnail: "https://i.ytimg.com/vi/gcB4ay_fO1k/maxresdefault.jpg",
+        author: "Lofi Girl",
+
+    }
+];
+
+export const videoGame = [
+    {
+        id: 0,
+        title: "Video Game Lofi & Chill Beats 🎮 24/7 Radio",
+        url: "https://www.youtube.com/watch?v=akW7qWS_p-g",
+        thumbnail: "https://i.ytimg.com/vi/akW7qWS_p-g/maxresdefault.jpg",
+        author: "GlitchxCity",
+    },
+];
+
+export const jazz = [
+    {
+        id: 0,
+        title: "[Playlist] 일하면서 틀어 놓기 좋은 재즈 플리 | Work & Sudy Jazz | Relaxing Background Music",
+        url: "https://www.youtube.com/watch?v=T-SU0bfyJfU",
+        thumbnail: "https://i.ytimg.com/vi/T-SU0bfyJfU/maxresdefault.jpg",
+        author: "Jazz"
+    }
+]
