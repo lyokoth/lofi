51a295e5983d1395f32336953e3bf42ee8bc7d8d
Lynette Okoth <lynokoth79@gmail.com>
2024-08-19 14:33:47 -0400

    update YT playlists

--- src/data/youtubePlaylistData.js
+++ src/data/youtubePlaylistData.js
@@ -1,15 +1,15 @@
 export const tokyo = [
     {
         id: 0,
-        title: "japan night drive - chill lofi hip hop to study / work / sleep + late night drive in japan ✨",
-        url: "https://www.youtube.com/watch?v=YvC4PTKoLww",
+        title: "tokyo night drive - lofi hiphop + chill + beats to sleep/relax/study to ✨",
+        url: "https://www.youtube.com/watch?v=Lcdi9O2XB4E",
         thumbnail: "https://i.ytimg.com/vi/1jXw9G7y3ow/maxresdefault.jpg",
         author: "TOKYO TONES",
     },
     {
         id: 1,
-        title: "🏎 tokyo night drive - synthwave radio / upbeat / chill 24/7",
-        url: "https://www.youtube.com/watch?v=KNJyQwgzrMg",
+        title: "[lofi] tokyo rain 🌧 lofi + rain sounds to relax / sleep / study",
+        url: "https://www.youtube.com/watch?v=p1Wa9whjoLw",
         thumbnail: "https://i.ytimg.com/vi/KNJyQwgzrMg/maxresdefault.jpg",
         author: "TOKYO TONES",
     },
@@ -18,15 +18,15 @@ export const tokyo = [
 export const lofiGirl = [
     {
         id: 0,
-        title: "lofi hip hop radio - beats to relax/study to",
-        url: "https://www.youtube.com/watch?v=5qap5aO4i9A",
+        title: "lofi hip hop radio📚- beats to relax/study to",
+        url: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
         thumbnail: "https://i.ytimg.com/vi/5qap5aO4i9A/maxresdefault.jpg",
         author: "Lofi Girl",
     },
     {
         id: 1,
-        title: "1980s Lofi",
-        url: "https://www.youtube.com/watch?v=gcB4ay_fO1k",
+        title: "Chill Study Corner ✨ Dreamy Work Space With Lofi Deeps Focus ~ Lofi hip hop radio",
+        url: "https://www.youtube.com/watch?v=uDTi0dxi_QM",
         thumbnail: "https://i.ytimg.com/vi/gcB4ay_fO1k/maxresdefault.jpg",
         author: "Lofi Girl",
 
@@ -50,5 +50,33 @@ export const jazz = [
         url: "https://www.youtube.com/watch?v=T-SU0bfyJfU",
         thumbnail: "https://i.ytimg.com/vi/T-SU0bfyJfU/maxresdefault.jpg",
         author: "Jazz"
+    }
+];
+
+export const hipHop = [
+    {
+        id: 0, 
+        title: "nujabes playlist / 비의계절,누자베스,작업실",
+        url: "https://www.youtube.com/watch?v=F14gbBf__ow",
+        thumbnail: "",
+        author: "Nujabes"
+    }
+];
+
+
+export const krnb = [
+    {
+        id: 0,
+        title: "24/7 이곳이 바로 힙스터 성지 😎 kozypop radio, k-pop, krnb, indie",
+        url: "https://www.youtube.com/watch?v=w0rhmlE_8-s",
+        thumbnail: "",
+        author: "KozyPop"
+    },
+    {
+        id: 1,
+        title: "24/7 Korean Underground Indie/R&B/Hip-hop Radio",
+        url: "https://www.youtube.com/watch?v=IUT1qAhMY4w",
+        thumbnail: "",
+        author: "Mellowbeat Seeker"
     }
 ]
